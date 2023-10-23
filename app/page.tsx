d211e82c8154ad248aa71cfdb2a7d42af932cb69
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-10-23 22:22:05 +0000

    nome

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Site School</title>
+  </head>
 
-export default function Home() {
-  return (
-<h1>tiruto</h1>
-  )
-}
+  <body>
+  
+  </body>;
