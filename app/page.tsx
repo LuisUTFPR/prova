f9e6eb1f729be47978cc97b0efde4cd21de034a9
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-10-23 22:24:11 +0000

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Site School</title>
-  </head>
+import Image from 'next/image'
+import { text } from 'stream/consumers'
 
-  <body>
-  
-  </body>;
+export default function Home() {
+  return (
+<h1>HealthCare </h1>
+
+
+  )
+}
