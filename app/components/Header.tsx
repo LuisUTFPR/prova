027e96ba5d975649d08ca41ce0d770387ec1063c
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-10-27 21:55:44 +0000

    aqwe

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,13 +8,12 @@ export default function Header() {
                 </h1>
                 <nav>
                   <ul className="flex space-x-4 text-white">
-                    <li>
-                    <a
-  href="#"
-  className="linetext-[white] bg-orange-500 px-8 py-[8px] rounded hover:bg-blue-600 hover:text-white"
->
-  Contact Us
-</a>
+                 
+                      <a href="#"
+                        className="linetext-[white] bg-orange-500 px-8 py-[8px] rounded hover:bg-blue-600 hover:text-white"
+                      >
+                        Contact Us
+                      </a>
                   </ul>
                 </nav>
               </div>
