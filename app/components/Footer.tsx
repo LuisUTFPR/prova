193d52888a7824483082a1296b50fcf0f6af9a78
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-10-28 01:04:57 +0000

    desisto 2 vezes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -82,17 +82,16 @@ export default function Footer() {
         <div className="px-4 py-6 bg-[#12AC8E] md:flex md:items-center md:justify-between">
         
         
-            <span className="text-sm text-white  sm:text-center"
-              >© 2023 <a href="#"> Copyright © 2023 Web Design Mastery. All rights reserved.
-            </span>
-            <a/>
-            <a href="">
-              <svg
-                  fill="white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 512 512"
-              >
+        <span className="text-sm text-white text-center">
+  © 2023 <a href="#">Copyright © 2023 Web Design Mastery</a>. All rights reserved.
+</span>
+<a href="">
+  <svg
+    fill="white"
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 512 512"
+  >
                   
                   <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
               </svg>
