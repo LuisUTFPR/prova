e0eb7370212ac426feeb82bb1a51d9d94f6d9fc7
LuisUTFPR <147564144+LuisUTFPR@users.noreply.github.com>
2023-10-28 00:03:16 +0000

    sandy e junior

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -4,15 +4,15 @@ export default function Section() {
       <header className="text-center">
         <h2 className="text-4xl font-bold ml-8 text-left">Our Special Service</h2>
       </header>
-      <div className="flex flex-wrap">
-        <div className="w-full sm:w-1/2 md:w-1/3 p-2 mb-4">
-          <div className="bg-white rounded-lg shadow-lg text-center">
+      <div className="flex flex-wrap -mx-2 mt-4">
+        <div className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
+          <div className="bg-white rounded-lg shadow-lg p-4">
             <img
-              src="/img/test1.svg"
+              src="/img/card-image1.jpg"
               alt="Card Image 1"
-              className="w-24 h-24 m-auto mt-4"
+              className="w-16 h-16 m-auto mb-4"
             />
-            <h3 className="text-xl font-bold mt-4">Laboratory Test</h3>
+            <h3 className="text-lg font-bold mb-2">Laboratory Test</h3>
             <p className="text-base my-2">
               Accurate Diagnostics, Swift Results: Experience top-notch Laboratory
               Testing at our facility.
@@ -22,14 +22,14 @@ export default function Section() {
             </div>
           </div>
         </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 p-2 mb-4">
-          <div className="bg-white rounded-lg shadow-lg text-center">
+        <div className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
+          <div className="bg-white rounded-lg shadow-lg p-4">
             <img
-              src="/img/test1.svg"
+              src="/img/card-image2.jpg"
               alt="Card Image 2"
-              className="w-24 h-24 m-auto mt-4"
+              className="w-16 h-16 m-auto mb-4"
             />
-            <h3 className="text-xl font-bold mt-4">Health Check</h3>
+            <h3 className="text-lg font-bold mb-2">Health Check</h3>
             <p className="text-base my-2">
               Our thorough assessments and expert evaluations help you stay
               proactive about your health.
@@ -39,14 +39,14 @@ export default function Section() {
             </div>
           </div>
         </div>
-        <div className="w-full sm:w-1/2 md:w-1/3 p-2 mb-4">
-          <div className="bg-white rounded-lg shadow-lg text-center">
+        <div className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
+          <div className="bg-white rounded-lg shadow-lg p-4">
             <img
-              src="/img/test3.svg"
+              src="/img/card-image3.jpg"
               alt="Card Image 3"
-              className="w-24 h-24 m-auto mt-4"
+              className="w-16 h-16 m-auto mb-4"
             />
-            <h3 className="text-xl font-bold mt-4">General Dentistry</h3>
+            <h3 className="text-lg font-bold mb-2">General Dentistry</h3>
             <p className="text-base my-2">
               Experience comprehensive oral care with Dentistry. Trust us to keep
               your smile healthy and bright.
@@ -60,3 +60,8 @@ export default function Section() {
     </section>
   );
 }
+
+
+
+
+
